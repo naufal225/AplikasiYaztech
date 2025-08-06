35bf66adebcf5817ddaaa2969161c27d4a9722a2
naufal225 <naufalmarufashrori225@gmail.com>
2025-08-06 14:19:34 +0700

    membuat controller

---
+++ app/Http/Controllers/ApproverController/OvertimeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ApproverController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OvertimeController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__.'/admin.php';
+require __DIR__.'/approver.php';
+require __DIR__.'/employee.php';
