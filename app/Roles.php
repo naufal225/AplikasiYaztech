c7fbf5c1617eb1a7e239579ca0303409268db193
naufal225 <naufalmarufashrori225@gmail.com>
2025-08-15 10:19:10 +0700

    nambahin role finance

--- app/Roles.php
+++ app/Roles.php
@@ -2,10 +2,17 @@
 
 namespace App;
 
+use Spatie\Permission\Contracts\Role;
+
 enum Roles : String
 {
     case Admin = "admin";
-    case Approver = "approver";
+    case Approver = "approver"; // ini team lead
     case Employee = "employee";
     case HR = "hr";
+    case Finance = "finance";
+
+    public static function values() {
+        return array_map(fn($role) => $role->value, self::cases());
+    }
 }
