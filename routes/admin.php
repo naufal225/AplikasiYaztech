e578025aa1a212bd0f13faaf3ceacbe10acf31ca
naufal225 <naufalmarufashrori225@gmail.com>
2025-08-21 11:22:11 +0700

    membuat profile untuk admin

--- routes/admin.php
+++ routes/admin.php
@@ -8,19 +8,18 @@ use App\Http\Controllers\AdminController\UserController;
 use App\Http\Controllers\AdminController\LeaveController;
 use App\Http\Controllers\AdminController\OfficialTravelController;
 use App\Http\Controllers\AdminController\OvertimeController;
+use App\Http\Controllers\AdminController\ProfileController;
 use App\Http\Controllers\AdminController\ReimbursementController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', function() {
+    Route::get('/', function () {
         return redirect()->route('admin.dashboard');
     });
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::resource('divisions', DivisionController::class);
-
-    Route::resource('customers', CustomerController::class);
 
     Route::resource('users', UserController::class);
 
@@ -45,4 +44,7 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->grou
     Route::get('/official-travels/export', [OfficialTravelController::class, 'export'])
         ->name('official-travels.export');
     Route::resource('official-travels', OfficialTravelController::class);
+
+    Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password');
+    Route::resource('profile', ProfileController::class);
 });
