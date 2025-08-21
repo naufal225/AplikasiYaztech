6ab355e55178ae1b81821092bd434c9387d56ae8
naufal225 <naufalmarufashrori225@gmail.com>
2025-08-21 10:51:13 +0700

    migrasi

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('role', Roles::values())->default(Roles::Employee->value);
+            $table->text('url_profile')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
