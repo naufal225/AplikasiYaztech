f012f644468e9d7d2505e6e717d9d0590e57c537
naufal225 <naufalmarufashrori225@gmail.com>
2025-09-23 09:42:03 +0700

    finish

--- app/Http/Controllers/PublicApprovalController.php
+++ app/Http/Controllers/PublicApprovalController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ApprovalLink;
 use App\Models\User;
-use App\Roles;
+use App\Enums\Roles;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
